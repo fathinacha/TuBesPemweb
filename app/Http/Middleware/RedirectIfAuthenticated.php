7d9a65569e6af2f43a471fae9dfd34f84b17553c
Ikhsan Ari Novianto <ikhsanarin9@gmail.com>
2024-06-05 18:51:44 +0700

    ikhsan

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,28 @@
+<?php
+
+// app/Http/Middleware/RedirectIfAuthenticated.php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->check()) {
+            return redirect('/login'); // Mengarahkan pengguna ke halaman login jika sudah terotentikasi
+        }
+
+        return $next($request);
+    }
+}
